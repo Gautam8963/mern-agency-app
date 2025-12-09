d99562d7e22d1abe5891c751d36254f5fb8131b8
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-12-09 23:57:34 +0530

    updated url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,22 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:5001/api', // Adjust if deployed
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// });
+
+// export default api;
+
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api', // Adjust if deployed
+    baseURL: import.meta.env.VITE_API_URL,  // <-- Dynamic base URL
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
 export default api;
+
